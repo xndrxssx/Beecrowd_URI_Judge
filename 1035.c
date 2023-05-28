2131bcdf5925926d45fb9b707f0ee55c126cff23
xndrxssx <acarvalho0710@gmail.com>
2023-05-28 12:43:18 -0300

    Teste de Seleção - https://www.beecrowd.com.br/judge/pt/problems/view/1035

---
+++ 1035.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    if (b>c)
+       {
+        if (d>a)
+            if ((c+d)>(a+b))
+                if (c>0)
+                    if (d>0)
+                        if (!(a%2))
+                        printf ("Valores aceitos\n");
+                        else
+                        printf("Valores nao aceitos\n");
+                    else
+                    printf("Valores nao aceitos\n");
+                else
+                printf("Valores nao aceitos\n");
+            else
+            printf("Valores nao aceitos\n");
+        else
+        printf("Valores nao aceitos\n");                
+        }
+    else
+    printf("Valores nao aceitos\n");
+
+
+    return 0;
+}
