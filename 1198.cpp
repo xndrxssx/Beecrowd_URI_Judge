7f57b020021ff57b0a9ccc94285296f00a20216d
xndrxssx <acarvalho0710@gmail.com>
2023-05-28 14:04:32 -0300

    O Bravo Guerreiro Hashmat - https://www.beecrowd.com.br/judge/pt/problems/view/1198

---
+++ 1198.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    long long int a, b, r;
+    while(cin>>a>>b){
+        r=a-b;
+            if (r<0)
+                r*=-1;
+        
+        cout<<r<<endl;
+    }
+    return 0;
+}
